9426a8d0c89a40337a19f8b8137417f42529ab13
Jesse Nieminen <jesse.nieminen@relex.fi>
2013-08-10 21:22:17 +0300

    Exported the portchecker script to its own file

---
+++ portchecker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#First checks out what is listening on the port gives as the first parameter, then checks what process that actually is.
+
+function portcheck() {
+  echo "LSOF OUTPUT FOR PORT "$1":"
+  sudo lsof -n -i4TCP:$1
+  PID=$(sudo lsof -n -i4TCP:$1 |grep -v 'PID' |awk '{print $2}')
+
+  echo "PS AUX RESULT FOR PID "$PID":"
+  sudo ps aux | grep $PID | grep -v 'grep'
+}
+
